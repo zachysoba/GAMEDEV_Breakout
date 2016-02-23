d30ab7b208c8dc897635ec6a1a251bcf67b1c9ef
zachysoba <zachraduban@gmail.com>
2016-02-23 15:49:45 +0800

    changes

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -18,71 +18,71 @@ Scene* HelloWorld::createScene()
 }
 
 // on "init" you need to initialize your instance
+
+
 bool HelloWorld::init()
 {
-    //////////////////////////////
-    // 1. super init first
+   
     if ( !Layer::init() )
     {
         return false;
     }
+
+	auto visibleSize = Director::getInstance()->getVisibleSize();
+	auto origin = Director::getInstance()->getVisibleOrigin();
+
+	
     
-    Size visibleSize = Director::getInstance()->getVisibleSize();
-    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+	edgeSp = Sprite::create();
+	auto boundBody = PhysicsBody::createEdgeBox(visibleSize, PHYSICSBODY_MATERIAL_DEFAULT, 3);
+	boundBody->getShape(0)->setRestitution(1.0f);
+	boundBody->getShape(0)->setFriction(0.0f); 
+	boundBody->getShape(0)->setDensity(1.0f); 
+	edgeSp->setPosition(Point(visibleSize.width / 2, visibleSize.height / 2)); 
+	edgeSp->setPhysicsBody(boundBody); 
+	boundBody->setContactTestBitmask(0x000001);
+	this->addChild(edgeSp); 
+	edgeSp->setTag(0); 
 
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
+	ball = Sprite::create("Ball.png", Rect(0, 0, 52, 52));
+	ball->setPosition(100, 100);
+	auto ballBody = PhysicsBody::createCircle(ball->getContentSize().width / 2.);
+	ballBody->getShape(0)->setRestitution(1.0f);
+	ballBody->getShape(0)->setFriction(0.0f);
+	ballBody->getShape(0)->setDensity(1.0f);
+	ballBody->setGravityEnable(false); 
+	Vect force = Vect(1010000.0f, 1010000.0f); 
+	ballBody->applyImpulse(force); 
+	ball->setPhysicsBody(ballBody); 
+	ballBody->setContactTestBitmask(0x000001);
+	ball->setTag(1);
+	this->addChild(ball);
 
-    // add a "close" icon to exit the progress. it's an autorelease object
-    auto closeItem = MenuItemImage::create(
-                                           "CloseNormal.png",
-                                           "CloseSelected.png",
-                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
-    
-	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
-                                origin.y + closeItem->getContentSize().height/2));
+	paddle = Sprite::create("Paddle.png");
+	auto paddleBody = PhysicsBody::createBox(paddle->getContentSize(), PHYSICSBODY_MATERIAL_DEFAULT);
+	paddleBody->getShape(0)->setRestitution(1.0f);
+	paddleBody->getShape(0)->setFriction(0.0f);
+	paddleBody->getShape(0)->setDensity(10.0f); 
+	paddleBody->setGravityEnable(false); 
+	paddleBody->setDynamic(false); 
+	paddle->setPosition(visibleSize.width / 2, 50); 
+	paddle->setPhysicsBody(paddleBody);
+		paddleBody->setContactTestBitmask(0x000001);
 
-    // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, NULL);
-    menu->setPosition(Vec2::ZERO);
-    this->addChild(menu, 1);
-
-    /////////////////////////////
-    // 3. add your codes below...
-
-    // add a label shows "Hello World"
-    // create and initialize a label
-    
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-    
-    // position the label on the center of the screen
-    label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - label->getContentSize().height));
-
-    // add the label as a child to this layer
-    this->addChild(label, 1);
-
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
-
-    // position the sprite on the center of the screen
-    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-
-    // add the sprite as a child to this layer
-    this->addChild(sprite, 0);
-    
-    return true;
-
+	ball->setTag(2);
+	this->addChild(paddle);
+	
+	
 	for (int i = 0; i < 5; i++) {
+
 		static int padding = 100;
+
 		auto block = Sprite::create("blocks.png");
 		auto blockBody = PhysicsBody::createBox(block->getContentSize(), PHYSICSBODY_MATERIAL_DEFAULT);
 		blockBody->getShape(0)->setDensity(10.0f);
 		blockBody->getShape(0)->setFriction(0.0f);
 		blockBody->getShape(0)->setRestitution(1.f);
 		blockBody->setDynamic(false);
-		// Create the distance even among the bricks
 		int xOffset = padding + block->getContentSize().width / 2 +
 			((block->getContentSize().width + padding)*i);
 		block->setPosition(xOffset, 450);
@@ -91,51 +91,30 @@ bool HelloWorld::init()
 		block->setTag(3);
 		this->addChild(block);
 	}
-
-	*Add ContactListener
-
-		auto dispatcher = Director::getInstance()->getEventDispatcher();
+	
+	
+	
+	auto touchListener = EventListenerTouchOneByOne::create();
+	touchListener->setSwallowTouches(true);
+    touchListener->onTouchBegan = CC_CALLBACK_2(HelloWorld::onTouchBegan, this);
+    touchListener->onTouchMoved = CC_CALLBACK_2(HelloWorld::onTouchMoved, this);
+    touchListener->onTouchEnded = CC_CALLBACK_2(HelloWorld::onTouchEnded, this);
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(touchListener, this);
+	
+	
+	auto dispatcher = Director::getInstance()->getEventDispatcher();	
 	auto contactListener = EventListenerPhysicsContact::create();
-	contactListener->onContactBegin = CC_CALLBACK_1(HelloWorld::onContactBegin, this);
-	dispatcher->addEventListenerWithSceneGraphPriority(contactListener, this);
-
-	*Build the function onContactBegin
-
-		bool HelloWorld::onContactBegin(PhysicsContact& contact)
-	{
-		// Get two collided object
-		auto spriteA = (Sprite*)contact.getShapeA()->getBody()->getNode();
-		auto spriteB = (Sprite*)contact.getShapeB()->getBody()->getNode();
-
-		// Check kinds of objects
-		int tagA = spriteA->getTag();
-		int tagB = spriteB->getTag();
-
-		if (tagA == 3) // is brick
-		{
-
-			this->removeChild(spriteA, true); // delete brick
-
-			//spriteA->removeFromParentAndCleanup(true);
-		}
-
-		if (tagB == 3)  // is brick
-		{
-			this->removeChild(spriteB, true); // delete brick
-
-			//spriteB->removeFromParentAndCleanup(true);
-		}
-
-		// If the ball collides with the floor and the coordinate Y of the ball is smaller than the paddle, Game Over happens
-		if ((tagA == 0 || tagB == 0)& (ball->getPositionY() <= paddle->getPositionY()))
-		{
-			auto gameOverScene = GameOverScene::create();
-			gameOverScene->getLayer()->getLabel()->setString("You Lose!");
-			Director::getInstance()->replaceScene(gameOverScene);
-		}
-
-		return true;
-	}
+	contactListener->onContactBegin = CC_CALLBACK_1(HelloWorld::onContactBegin, this);    
+    dispatcher->addEventListenerWithSceneGraphPriority(contactListener, this);
+	
+	
+	
+	
+	this->schedule(schedule_selector(HelloWorld::tick),0);
+	
+	
+    return true;
+}
 
 	This class GameOverScene is similar in the first project
 
