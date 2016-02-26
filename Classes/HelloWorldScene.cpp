934ef3f9247b7e3786f260f74218f42f3165fdfb
zachysoba <zachraduban@gmail.com>
2016-02-26 21:04:53 +0800

    Finished product
    - Finalized the basic components of the game
    - Added game over scene

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -1,14 +1,22 @@
- #include "HelloWorldScene.h"
+#include "HelloWorldScene.h"
+#include "GameOverScene.h"
 
 USING_NS_CC;
 
 Scene* HelloWorld::createScene()
 {
     // 'scene' is an autorelease object
-    auto scene = Scene::create();
+    auto scene = Scene::createWithPhysics();
+
+	scene->getPhysicsWorld()->setDebugDrawMask(PhysicsWorld::DEBUGDRAW_NONE);
+
+	Vect gravity(0.0f, 0.0f);
+	scene->getPhysicsWorld()->setGravity(gravity);
     
     // 'layer' is an autorelease object
     auto layer = HelloWorld::create();
+
+	layer->setPhyWorld(scene->getPhysicsWorld());
 
     // add layer as a child to scene
     scene->addChild(layer);
@@ -18,11 +26,9 @@ Scene* HelloWorld::createScene()
 }
 
 // on "init" you need to initialize your instance
-
-
 bool HelloWorld::init()
 {
-   
+
     if ( !Layer::init() )
     {
         return false;
@@ -35,30 +41,31 @@ bool HelloWorld::init()
     
 	edgeSp = Sprite::create();
 	auto boundBody = PhysicsBody::createEdgeBox(visibleSize, PHYSICSBODY_MATERIAL_DEFAULT, 3);
-	boundBody->getShape(0)->setRestitution(1.0f);
-	boundBody->getShape(0)->setFriction(0.0f); 
-	boundBody->getShape(0)->setDensity(1.0f); 
+	boundBody->getShape(0)->setRestitution(1.0f); 
+	boundBody->getShape(0)->setFriction(0.0f);
+	boundBody->getShape(0)->setDensity(10.0f);
 	edgeSp->setPosition(Point(visibleSize.width / 2, visibleSize.height / 2)); 
 	edgeSp->setPhysicsBody(boundBody); 
 	boundBody->setContactTestBitmask(0x000001);
-	this->addChild(edgeSp); 
-	edgeSp->setTag(0); 
+	this->addChild(edgeSp);
+	edgeSp->setTag(0);
+	
 
-	ball = Sprite::create("Ball.png", Rect(0, 0, 52, 52));
+	ball = Sprite::create("ball.png", Rect(0, 0, 18, 18));
 	ball->setPosition(100, 100);
 	auto ballBody = PhysicsBody::createCircle(ball->getContentSize().width / 2.);
 	ballBody->getShape(0)->setRestitution(1.0f);
 	ballBody->getShape(0)->setFriction(0.0f);
 	ballBody->getShape(0)->setDensity(1.0f);
 	ballBody->setGravityEnable(false); 
-	Vect force = Vect(1010000.0f, 1010000.0f); 
-	ballBody->applyImpulse(force); 
+	Vect force = Vect(101000.0f, 101000.0f); 
+	ballBody->applyImpulse(force);
 	ball->setPhysicsBody(ballBody); 
 	ballBody->setContactTestBitmask(0x000001);
 	ball->setTag(1);
 	this->addChild(ball);
 
-	paddle = Sprite::create("Paddle.png");
+	paddle = Sprite::create("paddle.png");
 	auto paddleBody = PhysicsBody::createBox(paddle->getContentSize(), PHYSICSBODY_MATERIAL_DEFAULT);
 	paddleBody->getShape(0)->setRestitution(1.0f);
 	paddleBody->getShape(0)->setFriction(0.0f);
@@ -67,31 +74,67 @@ bool HelloWorld::init()
 	paddleBody->setDynamic(false); 
 	paddle->setPosition(visibleSize.width / 2, 50); 
 	paddle->setPhysicsBody(paddleBody);
-		paddleBody->setContactTestBitmask(0x000001);
+	paddleBody->setContactTestBitmask(0x000001);
 
 	ball->setTag(2);
 	this->addChild(paddle);
 	
-	
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < 7; i++) {
 
-		static int padding = 100;
+		static int padding = 52;
 
-		auto block = Sprite::create("blocks.png");
-		auto blockBody = PhysicsBody::createBox(block->getContentSize(), PHYSICSBODY_MATERIAL_DEFAULT);
-		blockBody->getShape(0)->setDensity(10.0f);
-		blockBody->getShape(0)->setFriction(0.0f);
-		blockBody->getShape(0)->setRestitution(1.f);
-		blockBody->setDynamic(false);
-		int xOffset = padding + block->getContentSize().width / 2 +
-			((block->getContentSize().width + padding)*i);
-		block->setPosition(xOffset, 450);
-		blockBody->setContactTestBitmask(0x000001);
-		block->setPhysicsBody(blockBody);
-		block->setTag(3);
-		this->addChild(block);
+		auto brick = Sprite::create("brick.png");
+		auto brickBody = PhysicsBody::createBox(brick->getContentSize(), PHYSICSBODY_MATERIAL_DEFAULT);
+		brickBody->getShape(0)->setDensity(10.0f);
+		brickBody->getShape(0)->setFriction(0.0f);
+		brickBody->getShape(0)->setRestitution(1.f);
+		brickBody->setDynamic(false);
+		int xOffset = padding + brick->getContentSize().width / 2 + ((brick->getContentSize().width + padding)*i);
+		brick->setPosition(xOffset, 400);
+			
+		brickBody->setContactTestBitmask(0x000001);
+		brick->setPhysicsBody(brickBody);
+		brick->setTag(3);
+		this->addChild(brick);
 	}
-	
+
+	for (int i = 0; i < 7; i++) {
+
+		static int padding = 52;
+
+		auto brick = Sprite::create("brick.png");
+		auto brickBody = PhysicsBody::createBox(brick->getContentSize(), PHYSICSBODY_MATERIAL_DEFAULT);
+		brickBody->getShape(0)->setDensity(10.0f);
+		brickBody->getShape(0)->setFriction(0.0f);
+		brickBody->getShape(0)->setRestitution(1.f);
+		brickBody->setDynamic(false);
+		int xOffset = padding + brick->getContentSize().width / 2 + ((brick->getContentSize().width + padding)*i);
+		brick->setPosition(xOffset, 450);
+
+		brickBody->setContactTestBitmask(0x000001);
+		brick->setPhysicsBody(brickBody);
+		brick->setTag(3);
+		this->addChild(brick);
+	}
+
+	for (int i = 0; i < 7; i++) {
+
+		static int padding = 52;
+
+		auto brick = Sprite::create("brick.png");
+		auto brickBody = PhysicsBody::createBox(brick->getContentSize(), PHYSICSBODY_MATERIAL_DEFAULT);
+		brickBody->getShape(0)->setDensity(10.0f);
+		brickBody->getShape(0)->setFriction(0.0f);
+		brickBody->getShape(0)->setRestitution(1.f);
+		brickBody->setDynamic(false);
+		int xOffset = padding + brick->getContentSize().width / 2 + ((brick->getContentSize().width + padding)*i);
+		brick->setPosition(xOffset, 500);
+
+		brickBody->setContactTestBitmask(0x000001);
+		brick->setPhysicsBody(brickBody);
+		brick->setTag(3);
+		this->addChild(brick);
+	}
 	
 	
 	auto touchListener = EventListenerTouchOneByOne::create();
@@ -116,17 +159,72 @@ bool HelloWorld::init()
     return true;
 }
 
-	This class GameOverScene is similar in the first project
 
+bool HelloWorld::onTouchBegan(Touch* touch, Event* event)
+{		
+return true;
+}
+
+void HelloWorld::onTouchEnded(Touch* touch, Event* event)
+{
 
 }
 
+void HelloWorld::tick(float dt)
+{	
 
-void HelloWorld::menuCloseCallback(Ref* pSender)
+	bool isWin = true;
+	Vector<PhysicsBody*> bodies = m_world->getAllBodies();
+	for each(PhysicsBody* body in bodies)
+	{
+		if (body->getNode()->getTag() == 3)
+		{
+			isWin = false;
+		}
+	}
+	if (isWin == true)
+	{
+		auto gameOverScene = GameOverScene::create();
+		gameOverScene->getLayer()->getLabel()->setString("You Won!");
+		Director::getInstance()->replaceScene(gameOverScene);
+	}
+	
+}
+
+
+void HelloWorld::onTouchMoved(Touch* touch, Event* event){
+	Point touchLocation = touch->getLocation();
+	paddle->setPositionX(touchLocation.x);	
+}
+
+bool HelloWorld::onContactBegin(PhysicsContact& contact)
 {
-    Director::getInstance()->end();
 
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-    exit(0);
-#endif
+	auto spriteA = (Sprite*)contact.getShapeA()->getBody()->getNode();
+	auto spriteB = (Sprite*)contact.getShapeB()->getBody()->getNode();
+	int tagA = spriteA->getTag();
+	int tagB = spriteB->getTag();
+
+	if (tagA == 3)
+	{
+		this->removeChild(spriteA,true);
+	}
+
+	if (tagB == 3)
+	{
+		this->removeChild(spriteB,true); 
+	}
+
+	auto origin = Director::getInstance()->getVisibleOrigin();
+	CCLOG("BAll = %f",ball->getPositionY());
+	
+	
+	if ((tagA == 0 || tagB  == 0 )& (ball->getPositionY() <= paddle->getPositionY()))
+	{
+		auto gameOverScene = GameOverScene::create();
+		gameOverScene->getLayer()->getLabel()->setString("You Lost!");
+		Director::getInstance()->replaceScene(gameOverScene);
+	}
+	
+	return true;
 }
--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -3,17 +3,37 @@
 
 #include "cocos2d.h"
 
+USING_NS_CC;
+
 class HelloWorld : public cocos2d::Layer
 {
 public:
+
+	Sprite* ball;
+	Sprite* paddle;
+	Sprite* edgeSp;
+
+	PhysicsWorld* m_world;
+	
+	//LabelTTF *status_label;
+
+	void setPhyWorld(PhysicsWorld* world){ m_world = world; };
+
+	void onTouchMoved(Touch *touch, Event *event);
+	void onTouchEnded(Touch *touch, Event *event);
+	
+	bool onTouchBegan(Touch *touch, Event *event);
+	
+	bool onContactBegin(PhysicsContact& contact);
+	
+	void tick(float dt);
+
     static cocos2d::Scene* createScene();
 
-    virtual bool init();
+    virtual bool init();  
+	
+	//virtual void onEnter();
     
-    // a selector callback
-    void menuCloseCallback(cocos2d::Ref* pSender);
-    
-    // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
 };
 
